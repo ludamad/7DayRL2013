8285de4a45033735a0c766c2401dd5d70a78af45
Pat Goebel <putterson@gmail.com>
2013-03-11 20:43:00 -0400

    ScentMaps holds 2 ScentMap

--- scents.py
+++ scents.py
@@ -1,20 +1,32 @@
+from globals import world
 from geometry import *
 
+class ScentMaps:
+    def __init__(self, size):
+        self.trailmap = ScentMap(size)
+        self.resourcemap = ScentMap(size)
+    def step(self):
+        self.trailmap.decay()
+
 class ScentMap:
-    def __init__(self, world, size):
-        self.world = world
+    def __init__(self, size):
         self.size = size
-        self.trailmap = [ [ 0 for x in range(size.w) ]
+        self.map = [ [ 0 for x in range(size.w) ]
                         for y in range(size.h) ]
-        self.resmap = [ [ 0 for x in range(size.w) ]
-                        for y in range(size.h) ]
-        self.decay = 1
+        self.decayrate = 1
     def rect(self):
         return make_rect( Pos(0,0), self.size )
-    def step(self,world):
-        self.decay(self.trailmap)        
-    def decay(self, map):
-        for i in self.rect():
-            if map[i] >= self.decay:
-                map[i] = map[i] - self.decay
+    
+    # To allow for a = map[xy]
+    def __getitem__(self, xy):
+        return self.map[ xy.y ][ xy.x ]
+
+    # To allow for map[xy] = a
+    def __setitem__(self, xy, strength):
+        self.map[ xy.y ][ xy.x ] = strength
+
+    def decay(self):
+        for xy in self.rect().xy_values():
+            if self[xy] >= self.decayrate:
+                self[xy] = self[xy] - self.decayrate
         
